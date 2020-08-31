c03906a3af634e8e83427c981526738a698c95d8
ronny1982 <wegener.ronny@gmail.com>
2020-08-31 15:00:04 +0200

    spoof user agent

--- src/RemoteControllerConfiguration.ts
+++ src/RemoteControllerConfiguration.ts
@@ -44,7 +44,7 @@ export class RemoteControllerConfiguration implements IRemoteControllerConfigura
     private readonly _diskspace: number; // in Byte, must be larger than 60 * 1024 * 1024 * 1024
     private readonly _networkspeed: number;
     private readonly _controlServer: string = CONTROL_SERVER;
-    private readonly _identifier = `MangaDex@Cloud ${CLIENT_VERSION} (${CLIENT_BUILD}) - Powered by deno.land`;
+    private readonly _identifier = `Mangadex@Home Node ${CLIENT_VERSION} (${CLIENT_BUILD})`; // `MangaDex@Cloud ${CLIENT_VERSION} (${CLIENT_BUILD}) - Powered by deno.land`;
     private _hostname: string = 'localhost';
     private _port: number;
     private _imageServer: string = '';
