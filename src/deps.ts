47b9d4e8f2fa54c5461d5dd6f0cf3d25acaac0b3
ronny1982 <wegener.ronny@gmail.com>
2020-09-01 16:28:59 +0200

    add oak request to dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -2,6 +2,6 @@
 
 export { listenAndServeTLS } from 'https://deno.land/std/http/server.ts';
 export { ListenOptionsTls } from 'https://deno.land/x/oak/application.ts';
-export { Application as Oak, Context } from 'https://deno.land/x/oak/mod.ts';
+export { Application as Oak, Context, Request as ContextRequest } from 'https://deno.land/x/oak/mod.ts';
 export { assert, assertEquals, assertArrayContains } from 'https://deno.land/std/testing/asserts.ts';
 export { delay } from 'https://deno.land/std/async/delay.ts';
